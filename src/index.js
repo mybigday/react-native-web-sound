952a3788b36d3c7854974f20a5307a237fb89f84
jhen <developer@jhen.me>
2020-10-26 11:30:05 +0800

    Use function instead of class

--- src/index.js
+++ src/index.js
@@ -1,94 +1,90 @@
 import { Howl, Howler } from 'howler'
 
-export default class Sound {
-  static enable() {}
-
-  static enableInSilenceMode() {}
-
-  static setCategory() {}
-
-  static setMode() {}
-
-  static setSpeakerPhone() {}
-
-  constructor(asset, basePath, onError) {
-    this.sound = new Howl({
-      src: [asset],
-      onload: onError,
-      onloaderror: onError,
-    })
-  }
-
-  isLoaded = () => this.sound.state() === 'loaded'
-
-  play = () => {
-    if (!this.isLoaded()) return this
-    this.sound.play()
-    return this
-  }
-
-  pause = () => {
-    if (!this.isLoaded()) return this
-    this.sound.pause()
-    return this
-  }
-
-  stop = () => {
-    this.sound.stop()
-    return this
-  }
-
-  reset = () => this
-
-  release = () => {
-    this.stop()
-    return this
-  }
-
-  getNumberOfChannels = () => -1
-
-  getDuration = () => this.sound.duration()
-
-  getVolume = () => this.sound.volume()
-
-  setVolume = (volume) => {
-    this.sound.volume(volume)
-    return this
-  }
-
-  getSystemVolume = () => Howler.volume()
-
-  setSystemVolume = (v) => Howler.volume(v)
-
-  getNumberOfLoops = () => this.sound.loop()
-
-  setNumberOfLoops = (n) => {
-    this.sound.loop(n === -1)
-    return this
-  }
-
-  setSpeed = (v) => {
-    this.sound.rate(v)
-    return this
-  }
-
-  setPan = (v) => {
-    this.sound.stereo(v)
-    return this
-  }
-
-  getPan = () => this.sound.stereo()
-
-  getCurrentTime = () => this.sound.seek()
-
-  setCurrentTime = (v) => {
-    this.sound.seek(v)
-    return this
-  }
-
-  isPlaying = () => this.sound.playing()
-
-  setCategory = () => {}
-
-  setSpeakerphoneOn = () => {}
+export default function Sound(asset, basePath, onError) {
+  this.sound = new Howl({
+    src: [asset],
+    onload: onError,
+    onloaderror: onError,
+  })
 }
+
+Sound.enable = () => {}
+Sound.enableInSilenceMode = () => {}
+Sound.setCategory = () => {}
+Sound.setMode = () => {}
+Sound.setSpeakerPhone = () => {}
+
+Sound.prototype.isLoaded = function () {
+  return this.sound.state() === 'loaded'
+}
+
+Sound.prototype.play = function() {
+  if (!this.isLoaded()) return this
+  this.sound.play()
+  return this
+}
+
+Sound.prototype.pause = function() {
+  if (!this.isLoaded()) return this
+  this.sound.pause()
+  return this
+}
+
+Sound.prototype.stop = function() {
+  this.sound.stop()
+  return this
+}
+
+Sound.prototype.reset = function() { return this }
+
+Sound.prototype.release = function() {
+  this.stop()
+  return this
+}
+
+Sound.prototype.getNumberOfChannels = function() { return -1 }
+
+Sound.prototype.getDuration = function() { return this.sound.duration() }
+
+Sound.prototype.getVolume = function() { return this.sound.volume() }
+
+Sound.prototype.setVolume = function(volume) {
+  this.sound.volume(volume)
+  return this
+}
+
+Sound.prototype.getSystemVolume = () => Howler.volume()
+
+Sound.prototype.setSystemVolume = (v) => Howler.volume(v)
+
+Sound.prototype.getNumberOfLoops = function() { return this.sound.loop() ? -1 : 0 }
+
+Sound.prototype.setNumberOfLoops = function(n) {
+  this.sound.loop(n === -1)
+  return this
+}
+
+Sound.prototype.setSpeed = function(v) {
+  this.sound.rate(v)
+  return this
+}
+
+Sound.prototype.setPan = function(v) {
+  this.sound.stereo(v)
+  return this
+}
+
+Sound.prototype.getPan = function() { return this.sound.stereo() }
+
+Sound.prototype.getCurrentTime = function() { return this.sound.seek() }
+
+Sound.prototype.setCurrentTime = function(v) {
+  this.sound.seek(v)
+  return this
+}
+
+Sound.prototype.isPlaying = function() { return this.sound.playing() }
+
+Sound.prototype.setCategory = () => {}
+
+Sound.prototype.setSpeakerphoneOn = () => {}
